4350c36c5f7bf0cd19dc86c8545e037d941f8858
Mathew <Mathew@130.159.193.174>
2013-02-06 15:49:39 +0000

    The base AI now runs away from ghosts if they get too close

--- src/pacman/entries/pacman/MyPacMan.java
+++ src/pacman/entries/pacman/MyPacMan.java
@@ -1,8 +1,11 @@
 package pacman.entries.pacman;
 
+import java.lang.reflect.Array;
 import java.util.Random;
 
 import pacman.controllers.Controller;
+import pacman.game.Constants.DM;
+import pacman.game.Constants.GHOST;
 import pacman.game.Constants.MOVE;
 import pacman.game.Game;
 
@@ -17,17 +20,45 @@ public class MyPacMan extends Controller<MOVE>
 	
 	public MOVE getMove(Game game, long timeDue) 
 	{
-		//A very simple intial base. The controller determines possible moves
-		//and randomly chooses one as it's next move. Going back in the direction
-		//it came is not a valid move.
+		
+		//Simple AI which randomly decides which direction to go every tick.
+		//If a ghost gets too close it will try to move away from it.
 		MOVE lastMove = game.getPacmanLastMoveMade();
 		int currentNode = game.getPacmanCurrentNodeIndex();
+		MOVE[] posMoves = game.getPossibleMoves(currentNode, lastMove);
+		double closest = 10000;
+		int ghostIndex = 5;
+		GHOST[] ghosts = new GHOST[4];
+		ghosts[0] = GHOST.BLINKY;
+		ghosts[1] = GHOST.PINKY;
+		ghosts[2] = GHOST.INKY;
+		ghosts[3] = GHOST.SUE;
 		
-		MOVE[] pos = game.getPossibleMoves(currentNode, lastMove);
-		Random r = new Random();
-		int stop = r.nextInt(pos.length);
-		myMove = pos[stop];
+		//Find the closest ghost to ms pac-man
+		for(int i=0; i<ghosts.length; i++){
+			double temp = game.getDistance(currentNode, game.getGhostCurrentNodeIndex(ghosts[i]), DM.PATH);
 			
+			if(temp < closest){
+				closest = temp;
+				ghostIndex = i;
+				System.out.println(closest);
+			}
+		}
+		
+		//If the closest ghost is too close then move away from it.
+		//Otherwise pick a random move to make.
+		if(closest <= 10 && closest != -1){
+			myMove = game.getNextMoveAwayFromTarget(currentNode, ghostIndex, DM.PATH);
+		}
+		
+		else{
+			Random rg = new Random();
+			int stop = rg.nextInt(posMoves.length);
+			myMove = posMoves[stop];
+		}
+		
+		
 		return myMove;
 	}
+	
 }
