6751c1b6b2ce6b25093f0bd58dab80401b3cac77
Mathew <Mathew@130.159.193.146>
2013-02-08 13:43:03 +0000

    Started work on a searching method for myMove

--- src/pacman/entries/pacman/MyPacMan.java
+++ src/pacman/entries/pacman/MyPacMan.java
@@ -21,51 +21,35 @@ public class MyPacMan extends Controller<MOVE>
 	public MOVE getMove(Game game, long timeDue) 
 	{
 		
-		//Simple AI which randomly decides which direction to go every tick, unless
-		//a ghost gets too close in which case it will try to move away from or towards it
-		//depending on it's current edible state.
+		/*
+		 * What this needs to do:
+		 * Find the neighbouring nodes.
+		 * Evaluate them and find the best one.
+		 * Find the neighbours for that node.
+		 * Rinse repeat until time runs out or best node found
+		 */
 		MOVE lastMove = game.getPacmanLastMoveMade();
 		int currentNode = game.getPacmanCurrentNodeIndex();
-		MOVE[] posMoves = game.getPossibleMoves(currentNode, lastMove);
-		double closest = 10000;
-		int ghostIndex = 5;
+		int currentBest = 0;
 		GHOST[] ghosts = new GHOST[4];
 		ghosts[0] = GHOST.BLINKY;
 		ghosts[1] = GHOST.PINKY;
 		ghosts[2] = GHOST.INKY;
 		ghosts[3] = GHOST.SUE;
 		
-		//Find the closest ghost to ms pac-man
-		for(int i=0; i<ghosts.length; i++){
-			double temp = game.getDistance(currentNode, game.getGhostCurrentNodeIndex(ghosts[i]), DM.PATH);
-			
-			if(temp < closest){
-				closest = temp;
-				ghostIndex = i;
-				System.out.println(closest);
-			}
-		}
+		int[] neighbours = game.getNeighbouringNodes(currentNode, lastMove);
 		
-		//If the closest ghost is too close to Ms Pac-man:
-		//First check to see if the ghost can be eaten,
-		//If it can then try to eat it otherwise run away from it
-		//Otherwise pick a random move to make.
-		if(closest <= 10 && closest != -1 && game.isGhostEdible(ghosts[ghostIndex]) == false){
-			myMove = game.getNextMoveAwayFromTarget(currentNode, game.getGhostCurrentNodeIndex(ghosts[ghostIndex]), DM.PATH);
-		}
+		evaluateNeighbours(neighbours);
 		
-		else if(closest <= 10 && closest != -1 && game.isGhostEdible(ghosts[ghostIndex]) == true){
-			myMove = game.getNextMoveTowardsTarget(currentNode, game.getGhostCurrentNodeIndex(ghosts[ghostIndex]), DM.PATH);
-		}
 		
-		else{
-			Random rg = new Random();
-			int stop = rg.nextInt(posMoves.length);
-			myMove = posMoves[stop];
-		}
 		
+		myMove = game.getNextMoveTowardsTarget(currentNode, currentBest, lastMove, DM.PATH);
 		
 		return myMove;
+	}
+	
+	public void evaluateNeighbours(int[] neighbours){
+		
 	}
 	
 }
