7e9f64a117011305a73449545edfc0d5b3bc60b2
Mathew <Mathew@192.168.2.71>
2013-02-10 15:20:35 +0000

    3rd Attempt at committing...
    Initial commit:
    Implemented an initial search function which evaluates the neighbouring
    nodes and scores them, trying to pick the best one to move to.
    
    Extra:
    Started working on getting it to search through more than the
    neighbouring nodes.

--- src/pacman/entries/pacman/MyPacMan.java
+++ src/pacman/entries/pacman/MyPacMan.java
@@ -1,7 +1,6 @@
 package pacman.entries.pacman;
 
-import java.lang.reflect.Array;
-import java.util.Random;
+import java.util.Timer;
 
 import pacman.controllers.Controller;
 import pacman.game.Constants.DM;
@@ -28,28 +27,71 @@ public class MyPacMan extends Controller<MOVE>
 		 * Find the neighbours for that node.
 		 * Rinse repeat until time runs out or best node found
 		 */
+		
 		MOVE lastMove = game.getPacmanLastMoveMade();
 		int currentNode = game.getPacmanCurrentNodeIndex();
 		int currentBest = 0;
+		int[] neighbours = game.getNeighbouringNodes(currentNode);
+		int check = 0;
+		
+		while(check != 40){			
+		
+			check ++;
+			
+			//Find the best node out of those.
+			currentBest = evaluateNeighbours(game, neighbours);
+			
+			//Make the next move to be towards the best found node
+			myMove = game.getNextMoveTowardsTarget(currentNode, currentBest, lastMove, DM.PATH);
+			
+			//get the neighbours of the best node.
+			neighbours = game.getNeighbouringNodes(currentBest);
+			
+		}
+		
+		return myMove;
+	}
+	
+	public int evaluateNeighbours(Game game, int[] neighbours){
+		int bestFound = 0;
+		int bestScore = 0;
 		GHOST[] ghosts = new GHOST[4];
 		ghosts[0] = GHOST.BLINKY;
 		ghosts[1] = GHOST.PINKY;
 		ghosts[2] = GHOST.INKY;
 		ghosts[3] = GHOST.SUE;
 		
-		int[] neighbours = game.getNeighbouringNodes(currentNode, lastMove);
-		
-		evaluateNeighbours(neighbours);
-		
-		
-		
-		myMove = game.getNextMoveTowardsTarget(currentNode, currentBest, lastMove, DM.PATH);
-		
-		return myMove;
-	}
-	
-	public void evaluateNeighbours(int[] neighbours){
-		
+		for(int i = 0; i < neighbours.length; i++){
+			int temp = neighbours[i];
+			int tempScore = 0;
+			
+			for(int j = 0; j < ghosts.length; j++){
+				int ghost = game.getGhostCurrentNodeIndex(ghosts[j]);
+				
+					if(ghost == temp && game.isGhostEdible(ghosts[j]) != true){
+						tempScore = tempScore - 1000;
+				}
+					else if(ghost == temp && game.isGhostEdible(ghosts[j]) == true){
+						tempScore = tempScore + game.getGhostCurrentEdibleScore();
+					}
+			}
+			
+			if(game.isPillStillAvailable(temp) == true){
+				tempScore = tempScore + 10;
+			}
+			
+			if(game.isPowerPillStillAvailable(temp) == true){
+				tempScore = tempScore + 50;
+			}
+			
+			if(tempScore > bestScore){
+				bestScore = tempScore;
+				bestFound = temp;
+			}
+		}
+		System.out.println(bestFound);
+		System.out.println(bestScore);
+		return bestFound;
 	}
 	
 }
