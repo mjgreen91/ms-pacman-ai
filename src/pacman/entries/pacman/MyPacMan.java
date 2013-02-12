f65c0f91b974f2d3f25b6a17b5a3c4f5498edb1a
mjgreen91 <mjgreen91@gmail.com>
2013-02-12 17:23:58 +0000

    Restructured the search and moved it into the getMove method. Two problems still exist with the controller: - It will not run away from ghosts. - It takes the exact same path each time it runs, even after death when there are no pills left in that section.

--- src/pacman/entries/pacman/MyPacMan.java
+++ src/pacman/entries/pacman/MyPacMan.java
@@ -30,68 +30,84 @@ public class MyPacMan extends Controller<MOVE>
 		
 		MOVE lastMove = game.getPacmanLastMoveMade();
 		int currentNode = game.getPacmanCurrentNodeIndex();
-		int currentBest = 0;
-		int[] neighbours = game.getNeighbouringNodes(currentNode);
+		int currentBest = currentNode;
+		int bestScore = game.getScore();
+		int projectedScore = bestScore;
+		MOVE[] posMoves = game.getPossibleMoves(currentNode, lastMove);
+		GHOST[] ghosts = new GHOST[4];
+			ghosts[0] = GHOST.BLINKY;
+			ghosts[1] = GHOST.PINKY;
+			ghosts[2] = GHOST.INKY;
+			ghosts[3] = GHOST.SUE;
+		boolean tooClose = false;
+		GHOST closestG = null;
 		int check = 0;
 		
 		while(check != 40){			
-		
+		//This is in place of a timer just now
 			check ++;
 			
-			//Find the best node out of those.
-			currentBest = evaluateNeighbours(game, neighbours);
+			//Search through all possible moves.
+			for(int i = 0; i < posMoves.length; i++){
+				int temp = game.getNeighbour(currentNode, posMoves[i]);;
+				int tempScore = 0;
 			
-			//Make the next move to be towards the best found node
+			//Find the best move out of those.
+				
+				//Searches for ghosts in the current node
+				for(int j = 0; j < ghosts.length; j++){
+					int ghost = game.getGhostCurrentNodeIndex(ghosts[j]);
+					
+						if(ghost == temp && game.isGhostEdible(ghosts[j]) != true){
+							tempScore = tempScore - 1000;
+					}
+						else if(ghost == temp && game.isGhostEdible(ghosts[j]) == true){
+							tempScore = tempScore + game.getGhostCurrentEdibleScore();
+						}
+						
+						else if(game.getDistance(currentNode, ghost, DM.PATH) < 10){
+							tooClose = true;
+							closestG = ghosts[j];
+						}
+				}
+				//checks if node has a pill in it. 
+				if(game.isPillStillAvailable(temp) == true){
+					tempScore = tempScore + 10;
+				}
+				
+				//Checks if there is a powerpill in the node.
+				//Would like to add clause that it doesn't go for
+				//powerpill if ghosts are still edible.
+				if(game.isPowerPillStillAvailable(temp) == true){
+					tempScore = tempScore + 50;
+				}
+				
+				if(tempScore > projectedScore){
+					projectedScore = tempScore;
+					currentBest = temp;
+				}
+				
+			}
+			
+			bestScore = projectedScore;
+			System.out.println("Best node: " + currentBest);
+			System.out.println("Best score: " + bestScore);
+			
+			//get the possible moves from the best node.
+			posMoves = game.getPossibleMoves(currentBest);
+			
+		}
+		//Make the next move to be towards the best found node
+		//Or away from the closest ghost.
+		if(tooClose == true){
+			myMove = game.getNextMoveAwayFromTarget(currentNode, game.getGhostCurrentNodeIndex(closestG), DM.PATH);
+		}
+		else{
 			myMove = game.getNextMoveTowardsTarget(currentNode, currentBest, lastMove, DM.PATH);
-			
-			//get the neighbours of the best node.
-			neighbours = game.getNeighbouringNodes(currentBest);
-			
 		}
 		
 		return myMove;
 	}
+
 	
-	public int evaluateNeighbours(Game game, int[] neighbours){
-		int bestFound = 0;
-		int bestScore = 0;
-		GHOST[] ghosts = new GHOST[4];
-		ghosts[0] = GHOST.BLINKY;
-		ghosts[1] = GHOST.PINKY;
-		ghosts[2] = GHOST.INKY;
-		ghosts[3] = GHOST.SUE;
-		
-		for(int i = 0; i < neighbours.length; i++){
-			int temp = neighbours[i];
-			int tempScore = 0;
-			
-			for(int j = 0; j < ghosts.length; j++){
-				int ghost = game.getGhostCurrentNodeIndex(ghosts[j]);
-				
-					if(ghost == temp && game.isGhostEdible(ghosts[j]) != true){
-						tempScore = tempScore - 1000;
-				}
-					else if(ghost == temp && game.isGhostEdible(ghosts[j]) == true){
-						tempScore = tempScore + game.getGhostCurrentEdibleScore();
-					}
-			}
-			
-			if(game.isPillStillAvailable(temp) == true){
-				tempScore = tempScore + 10;
-			}
-			
-			if(game.isPowerPillStillAvailable(temp) == true){
-				tempScore = tempScore + 50;
-			}
-			
-			if(tempScore > bestScore){
-				bestScore = tempScore;
-				bestFound = temp;
-			}
-		}
-		System.out.println(bestFound);
-		System.out.println(bestScore);
-		return bestFound;
-	}
-	
-}
+} 
